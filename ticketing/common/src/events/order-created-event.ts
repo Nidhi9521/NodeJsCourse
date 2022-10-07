b91b736a8210216d5895d3dcbde527be33ee7000
nidhi.gokani <gokaninidhi9521@gmail.com>
2022-10-07 13:01:56 +0530

    Upadtes

--- ticketing/common/src/events/order-created-event.ts
+++ ticketing/common/src/events/order-created-event.ts
@@ -5,6 +5,7 @@ export interface OrderCreatedEvent {
     data: {
         id:string;
         userId: string;
+        version:number;
         status: OrderStatus;
         expiresAt: Date;
         ticket: {
